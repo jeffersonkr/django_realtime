28727d2368d564f5570c88d1e4449670a900e8a5
Jefferson <jeffersonkr@hotmail.com>
2019-10-25 14:27:29 -0300

    removed function

--- core/views.py
+++ core/views.py
@@ -10,20 +10,3 @@ def room(request, room_name):
     return render(request, 'room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
-
-def subscribe_once(mac_address):
-    hostname = "192.168.10.198"
-    msg = subscribe.simple(
-        topics=f'monitoramento/{mac_address}', 
-        retained=True, 
-        hostname=hostname,
-        port=1883, 
-        keepalive=60, 
-        auth={
-            'username': 'iot-autodoc',
-            'password': 'IOTautodoc19!'
-            }
-        )
-    print(msg)
-
-    return msg.payload.decode('utf-8')
